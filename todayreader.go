fb57e8bce31770f23f1f56eaadfdaa2a0ba9bd64
Chang Sau Sheong <sausheong@gmail.com>
2014-07-27 23:32:27 +0800

    do not panic

--- todayreader.go
+++ todayreader.go
@@ -15,7 +15,7 @@ import (
 func failOnError(err error, msg string) {
   if err != nil {
     log.Fatalf("%s: %s", msg, err)
-    panic(fmt.Sprintf("%s: %s", msg, err))
+    // panic(fmt.Sprintf("%s: %s", msg, err))
   }
 }
 
